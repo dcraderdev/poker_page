750b11567312c001d958db5d5c532b73be1a48aa
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-08-15 19:35:07 -0700

    updated goals
    updated goals

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hi");
+// Poker Page!!! All things poker coding and algos
+
+const board = [[a,d],[q,d],[10,h]]
+
+function isPair(board, hand) {}
+function isTwoPair(board, hand) {}
+function isThreeOfKind(board, hand) {}
+function isStraight(board, hand) {}
+function isFlush(board, hand) {}
+function isFullHouse(board, hand) {}
+function isFourOfKind(board, hand) {}
+function isStraightFlush(board, hand) {}
+function isRoyalFlush(board, hand) {}
+
+
+
+
+
+function outsWithoutSuits(board, hand) {}
+  
+
+console.log([[a,d],[q,d],[10,h], [2,7]);
