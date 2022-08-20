75802af5b71bdc30d270446a17a6695b7c88db96
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-08-20 00:18:30 -0700

    app academy prep, will delete later
    app academy prep, will delete later

--- cleanscript.js
+++ cleanscript.js
@@ -439,3 +439,27 @@ function isRoyalFlush(board, hand) {}
 
 function outsWithoutSuits(board, hand) {}
 
+
+
+
+
+
+function wordWithinArray(array, word) {
+  return (array.indexOf(word) >= 0)
+};
+
+console.log(
+wordWithinArray(["apple", "banana", "caramel", "chocolate"], "apple")
+); //=> true
+
+console.log(wordWithinArray(
+["dog", "cat", "camel", "bird"], "camel")
+); //=> true
+
+console.log(
+wordWithinArray(["apple", "banana", "caramel", "chocolate"], "pineapple")
+); //=> false
+
+console.log(wordWithinArray(
+["dog", "cat", "camel", "bird"], "panther")
+); //=> false
