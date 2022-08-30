ac8ce0c7dbbb4e0badf688d0de254755acc029f5
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-08-30 13:30:13 -0700

    data structures practice
    data structures practice

--- datastructures.js
+++ datastructures.js
@@ -265,7 +265,7 @@ console.log(rest2);
 // 4. During the game,Bayern Munich(team1)used 3 substitute players.So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
 // 5. Based on the game.odds object, create one variable for each odd(called 'team1', 'draw' and 'team2')
 // 6. Write a function('printGoals')that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-// 7. The team ith the lower odd is more likely to win.Printtotheconsolewhich team is more likely to win, without using an if/else statement or the ternary operator.
+// 7. The team with the lower odd is more likely to win.Printtotheconsolewhich team is more likely to win, without using an if/else statement or the ternary operator.
 // Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 // GOOD LUCK 😀
 
@@ -320,3 +320,36 @@ console.log(players2);
 const [gk, ...fieldPlayers] = players1
 console.log(gk);
 console.log(fieldPlayers);
+
+// 3.
+const allPLayers = [...players1, ...players2]
+console.log(allPLayers);
+
+// 4. 
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']
+console.log(players1Final);
+
+
+ // 5. Based on the game.odds object, create one variable for each odd(called 'team1', 'draw' and 'team2')
+
+ const {odds: {team1, x: draw, team2}} = game
+
+ console.log(team1);
+ console.log(draw);
+ console.log(team2);
+
+ // 6. Write a function('printGoals')that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+ const printGoals = function (...names) {
+   console.log(`${names.length} goals were scored by`);
+  console.log(...names);
+ }
+
+printGoals('joey','chris','issac','henry','rich');
+
+ // 7. The team with the lower odd is more likely to win.Printtotheconsolewhich team is more likely to win, without using an if/else statement or the ternary operator.
+
+team1 < team2 && console.log('team1 is gonna win it!');
+team1 > team2 && console.log('team2 is gonna win it!');
+
+
--- practice.js
+++ practice.js
@@ -2484,3 +2484,7 @@ let maxValue = function(arr) {
 console.log(maxValue([4, 6, 3, 5, 42, 4])); // 42
 console.log(maxValue([-2, -3, -7, 3 ])); // 3
 console.log(maxValue([])); // null
+
+
+
+
